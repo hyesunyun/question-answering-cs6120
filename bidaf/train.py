f92eeee03dd892e95b879d69e23c8f1b1aa5da13
hyesunyun <hyesunyun09@gmail.com>
2021-04-26 10:56:28 -0400

    fix evaluate error

--- bidaf/train.py
+++ bidaf/train.py
@@ -194,12 +194,12 @@ def evaluate(model, data_loader, device, eval_file, max_len, use_squad_v2):
 
             # Forward
             if args.name == 'character' or args.name == 'combined':
-                    cc_idxs = cc_idxs.to(device)
-                    qc_idxs = qc_idxs.to(device)
-                    log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
-                else:
-                    log_p1, log_p2 = model(cw_idxs, qw_idxs)
-                    
+                cc_idxs = cc_idxs.to(device)
+                qc_idxs = qc_idxs.to(device)
+                log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
+            else:
+                log_p1, log_p2 = model(cw_idxs, qw_idxs)
+
             y1, y2 = y1.to(device), y2.to(device)
             loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
             nll_meter.update(loss.item(), batch_size)

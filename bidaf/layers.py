e64f11f73a75c14d592b12292a46356320ff3879
hyesunyun <hyesunyun09@gmail.com>
2021-04-27 11:33:17 -0400

    new coattention layer

--- bidaf/layers.py
+++ bidaf/layers.py
@@ -289,23 +289,71 @@ class BiDAFAttention(nn.Module):
 
         return s
 
+# class Coattention(nn.Module):
+#     """Coattention Layer.
+
+#     Based on the paper:
+#     "Dynamic Coattention Networks For Question Answering"
+#     by Caiming Xiong, Victor Zhong, Richard Socher
+#     (https://arxiv.org/abs/1611.01604).
+
+#     Coattention computes attention in two directions and involves a second-level
+#     attention computation (attending over representations that are themselves
+#     attention outputs:
+#     The first level attention is the C2Q attention which gets concatenated with
+#     the second-level attention outputs. This is then fed through a bidirectional
+#     LSTM.
+
+#     The output has shape (batch_size, context_len, 2 * hidden_size).
+
+#     Args:
+#         hidden_size (int): Size of hidden activations.
+#         drop_prob (float): Probability of zero-ing out activations.
+#     """
+#     def __init__(self, hidden_size, drop_prob=0.1):
+#         super(Coattention, self).__init__()
+#         self.drop_prob = drop_prob
+#         self.hidden_size = hidden_size
+#         # self.encoder = RNNEncoder(input_size=6*hidden_size,
+#         #                              hidden_size=hidden_size,
+#         #                              num_layers=1,
+#         #                              drop_prob=drop_prob)
+#         self.encoder = RNNEncoder(input_size=4*hidden_size,
+#                                      hidden_size=hidden_size,
+#                                      num_layers=1,
+#                                      drop_prob=drop_prob)
+#         self.q_linear = nn.Linear(hidden_size, hidden_size)
+#         self.dropout = nn.Dropout(p=drop_prob)
+
+#     def forward(self, c, q, c_mask, q_mask):
+
+#         # second version based on description in project description pdf
+#         qprime = torch.tanh(self.q_linear(q.view(-1, self.hidden_size))).view(q.size())
+#         c_t = torch.transpose(c, 1, 2)
+#         L = torch.bmm(qprime, c_t)
+#         Alpha = F.softmax(L, dim=1)
+#         a = torch.bmm(torch.transpose(Alpha, 1, 2), qprime)
+#         Beta = F.softmax(L, dim=2)
+#         b = torch.bmm(Beta, c)
+#         s = torch.bmm(torch.transpose(Alpha, 1, 2), b)
+#         bilstm_in = torch.cat((s, a), 2)
+#         bilstm_in = self.dropout(bilstm_in)
+#         U = self.encoder(bilstm_in, c_mask.sum(-1))
+#         return U
+
 class Coattention(nn.Module):
     """Coattention Layer.
-
     Based on the paper:
     "Dynamic Coattention Networks For Question Answering"
     by Caiming Xiong, Victor Zhong, Richard Socher
     (https://arxiv.org/abs/1611.01604).
-
     Coattention computes attention in two directions and involves a second-level
     attention computation (attending over representations that are themselves
     attention outputs:
     The first level attention is the C2Q attention which gets concatenated with
     the second-level attention outputs. This is then fed through a bidirectional
     LSTM.
-
     The output has shape (batch_size, context_len, 2 * hidden_size).
-
     Args:
         hidden_size (int): Size of hidden activations.
         drop_prob (float): Probability of zero-ing out activations.
@@ -314,11 +362,7 @@ class Coattention(nn.Module):
         super(Coattention, self).__init__()
         self.drop_prob = drop_prob
         self.hidden_size = hidden_size
-        # self.encoder = RNNEncoder(input_size=6*hidden_size,
-        #                              hidden_size=hidden_size,
-        #                              num_layers=1,
-        #                              drop_prob=drop_prob)
-        self.encoder = RNNEncoder(input_size=4*hidden_size,
+        self.encoder = RNNEncoder(input_size=6*hidden_size,
                                      hidden_size=hidden_size,
                                      num_layers=1,
                                      drop_prob=drop_prob)
@@ -327,18 +371,28 @@ class Coattention(nn.Module):
 
     def forward(self, c, q, c_mask, q_mask):
 
-        # second version based on description in project description pdf
-        qprime = torch.tanh(self.q_linear(q.view(-1, self.hidden_size))).view(q.size())
-        c_t = torch.transpose(c, 1, 2)
-        L = torch.bmm(qprime, c_t)
-        Alpha = F.softmax(L, dim=1)
-        a = torch.bmm(torch.transpose(Alpha, 1, 2), qprime)
-        Beta = F.softmax(L, dim=2)
-        b = torch.bmm(Beta, c)
-        s = torch.bmm(torch.transpose(Alpha, 1, 2), b)
-        bilstm_in = torch.cat((s, a), 2)
+        # q encoding projection
+        Qprime = torch.tanh(self.q_linear(q.view(-1, self.hidden_size))).view(q.size()) #B x n + 1 x l
+
+        # co attention
+        C_t = torch.transpose(c, 1, 2) #B x l x m + 1
+        L = torch.bmm(Qprime, C_t) # L = B x n + 1 x m + 1
+
+        A_Q_ = F.softmax(L, dim=1) # B x n + 1 x m + 1
+        A_Q = torch.transpose(A_Q_, 1, 2) # B x m + 1 x n + 1
+        C_Q = torch.bmm(C_t, A_Q) # (B x l x m + 1) x (B x m x n + 1) => B x l x n + 1
+
+        A_C = F.softmax(L, dim=2)  # B x n + 1 x m + 1
+        Q_t = torch.transpose(Qprime, 1, 2)  # B x l x n + 1
+        C_C = torch.bmm(torch.cat((Q_t, C_Q), 1), A_C) # (B x l x n+1 ; B x l x n+1) x (B x n +1x m+1) => B x 2l x m + 1
+
+        C_C_t = torch.transpose(C_C, 1, 2)  # B x m + 1 x 2l
+
+        # BiLSTM
+        bilstm_in = torch.cat((C_C_t, c), 2) # B x m + 1 x 3l
         bilstm_in = self.dropout(bilstm_in)
-        U = self.encoder(bilstm_in, c_mask.sum(-1))
+        #?? should it be d_lens + 1 and get U[:-1]
+        U = self.encoder(bilstm_in, c_mask.sum(-1)) #B x m x 2l
         return U
 
 class BiDAFOutput(nn.Module):
